f6bd53ec59579c11415e864a50e3ce96b9431dd1
JonnyJiang <JonnyJiang@gmail.com>
2021-05-04 12:29:27 +0800

    2

--- src/Test.java
+++ src/Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hhhh");
         System.out.println("aaaaa");
+        System.out.println("hot-fix");
     }
 }
